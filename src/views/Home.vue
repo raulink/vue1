78c05cfd30954a50640589ae4ed789a012a5a2c8
raulink <mc.raul86@gmail.com>
2021-04-03 09:47:22 -0400

    commit con vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>    <Titulo texto="Otro Titulo "/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Titulo texto="Otro Titulo " />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import Titulo from '../components/Titulo.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Titulo from "../components/Titulo.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld, Titulo  
-  }
-}
+    HelloWorld,
+    Titulo,
+  },
+};
 </script>
